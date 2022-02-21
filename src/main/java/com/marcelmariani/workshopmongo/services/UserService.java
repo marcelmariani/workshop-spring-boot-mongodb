467f523abb28f4142bb452894e09107c9a3505d0
marcelmariani <marcel.vazmariani@gmail.com>
2022-02-20 22:17:58 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marcelmariani/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.marcelmariani.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelmariani.workshopmongo.domain.User;
+import com.marcelmariani.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll(){
+		return repo.findAll();
+		
+	}
+	
+}
