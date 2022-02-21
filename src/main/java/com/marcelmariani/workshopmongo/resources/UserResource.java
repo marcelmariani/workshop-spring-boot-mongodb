76a46405bd39ac739f1e89857e5aaa9796a27880
marcelmariani <marcel.vazmariani@gmail.com>
2022-02-21 13:14:17 -0300

    Delecao de usuário com Delete

--- src/main/java/com/marcelmariani/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcelmariani/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,17 +38,16 @@ public class UserResource {
                 .orElse(ResponseEntity.notFound().build());
     }
     
-
    	@PostMapping
   	public User create(@RequestBody User objDto){
    		return service.save(objDto);
    	}
    	
-    /*@RequestMapping(method=RequestMethod.POST)
-    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
-    	User obj = service. .fromDTO(objDto);
-    	obj = service.insert(obj);
-    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-    	return ResponseEntity.created(uri).build();
-    }*/
+   	@DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+   		service.deleteById(id);
+   		return ResponseEntity.noContent().build();
+    }
+   	
+   	
 }
--- src/main/java/com/marcelmariani/workshopmongo/services/UserService.java
+++ src/main/java/com/marcelmariani/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.marcelmariani.workshopmongo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.marcelmariani.workshopmongo.domain.User;
@@ -22,6 +23,10 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		this.repo.findById(id).map(user -> ResponseEntity.ok(user)).orElse(ResponseEntity.notFound().build());
+		repo.deleteById(id);
+	}
 
 }
